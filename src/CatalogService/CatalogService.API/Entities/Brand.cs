53bd77da62b7c0b6bc7951c39d4ab98af0babddc
Shile <dangvtn2004@gmail.com>
2025-09-29 09:48:48 +0700

    Use SharedKernel for entities

--- src/CatalogService/CatalogService.API/Entities/Brand.cs
+++ src/CatalogService/CatalogService.API/Entities/Brand.cs
@@ -1,14 +1,13 @@
+using SharedKernel.Domain.Common.Entities;
+
 namespace CatalogService.Entities
 {
-    public class Brand
+    public class Brand : AuditableEntity<int>
     {
-        public int BrandId { get; set; }
         public string BrandName { get; set; } = null!;
         public string? BrandDescription { get; set; }
         public string? LogoUrl { get; set; }
         public string? WebsiteUrl { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
--- src/CatalogService/CatalogService.API/Entities/Product.cs
+++ src/CatalogService/CatalogService.API/Entities/Product.cs
@@ -1,12 +1,11 @@
+using SharedKernel.Domain.Common.Entities;
+
 namespace CatalogService.Entities
 {
-    public class Product
+    public class Product : AuditableEntity<int>
     {
-        public string ProductId { get; set; } = null!;
         public string ProductName { get; set; } = null!;
         public string? ProductDescription { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         // Foreign keys
         public int? CategoryId { get; set; }
