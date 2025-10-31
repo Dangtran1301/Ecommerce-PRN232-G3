30509f79a9224a14bc537e0dcfa4ef494c56ce0f
dullahan <dangdeptrai1212@gmail.com>
2025-10-31 19:06:26 +0700

    feat: CRUD User

---
+++ src/AuthService/AuthService.API/Interfaces/IClaimsService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace AuthService.API.Interfaces;
+
+public interface IClaimsService
+{
+    ClaimsPrincipal? GetPrincipalFromToken(string token);
+    string? GetUserId(string token);
+    string? GetEmail(string token);
+    string? GetFullName(string token);
+    string? GetRole(string token);
+
+    ClaimsPrincipal? CurrentUser { get; }
+    string? CurrentUserId { get; }
+    string? CurrentUserEmail { get; }
+    string? CurrentUserFullName { get; }
+    string? CurrentUserRole { get; }
+}
